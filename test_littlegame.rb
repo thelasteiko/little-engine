2f0dd67178cf41591cb091faf71a1f345494b466
Melinda Robertson <memre911@gmail.com>
2017-07-13 06:29:02 -0700

    Adding convenience methods to graphics for drawing.
    Added a line algorithm to supplement the inconsistent OpenGL version.
    Need to finish methods for drawing paths.

--- test_littlegame.rb
+++ test_littlegame.rb
@@ -3,16 +3,22 @@ require_relative "littlegame.rb"
 
 class TestObject < Little::Object
 	include Little::Focusable
+	include Little::Shapeable
+	
 	def initialize (game, scene)
 		super game, scene
 		@point = Little::Point.new(20,20)
 		#print "Created object\n"
+		@path = Little::Path.new([20,20,50,20,50,50,20,50,20,20])
 	end
 	
 	def draw (graphics, tick)
 		#Gosu::draw_rect(20,20,50,50,Gosu::Color::WHITE)
 		#print "drawing object\n"
-		graphics.rect(point, 50, 50)
+		#graphics.rect(point, 50, 50)
+		#graphics.pixel(point)
+		graphics.path(path)
+		#graphics.line(point, Little::Point.new(30,50))
 	end
 end
 
--- v2/littlegraphics.rb
+++ v2/littlegraphics.rb
@@ -26,10 +26,76 @@ module Little
         end
     end
     
+    class Path
+		attr_accessor	:points
+		def initialize (points=[])
+			if points.empty?
+				@points = points
+			else
+				@points = Array.new
+				i = 1
+				while i < points.size
+					@points.push(Little::Point.new(points[i-1],points[i]))
+					i += 2
+				end
+			end
+		end
+		def size
+			points.size
+		end
+		def convert_to_lines
+			path_set = []
+			i = 1
+			while i < points.size
+				
+			end
+		end
+		# Creates a list of points defining a line using Bresenham's
+		# line algorithm.
+		def self.bresenham_line(p1,p2)
+			line = []
+			dx = p2.x - p1.x
+			dy = p2.y - p1.y
+			if (dx == 0) # vertical line
+				y = p1.y
+				if p1.y > p2.y
+					while y > p2.y
+						line.push (Little::Point.new(p1.x,y)
+						y += 1
+					end
+				elsif p2.y > p1.y
+					while y < p2.y
+						line.push(Little::Point.new(p1.x,y)
+						y -= 1
+					end
+				end
+				return line
+			end
+			derr = (dy/dx).abs
+			error = derr - 0.5
+			y = p1.y
+			for x in p1.x..p2.x
+				line.push(Little::Point.new(x,y))
+				error += derr
+				if error >= 0.5
+					y += 1
+					error -= 1.0
+				end
+			end
+			return line
+		end
+    end
+    
     module Focusable
         def point
             @point ||= Little::Point.new
         end
+    end
+    
+    module Shapeable
+		def path
+			@path ||= Little::Path.new
+		end
     end
     # points are clockwise
     class Graphics
@@ -38,16 +104,42 @@ module Little
             @game = game
             @camera = camera
         end
-		def line (point1, point2, color=DEFAULT_COLOR)
+        # Uses the OpenGL method to draw the line. This has inconsistent
+        # behavior.
+		def line_ogl (point1, point2, color=DEFAULT_COLOR)
 			p1 = @camera.translate(point1)
 			p2 = @camera.translate(point2)
 			Gosu::draw_line(p1.x,p1.y,color,p2.x,p2.y,color)
+		end
+		# Creates a line between two points using the Bresenham line
+		# algorithm and draws the pixels individually.
+		def line (point1, point2, color=DEFAULT_COLOR)
+			l = Path::bresenham_line(point1,point2)
+			l.each do |i|
+				pixel(i,color)
+			end
 		end
 		def rect (point, width, height, color=DEFAULT_COLOR)
 			#print "into drawing rect \n"
 			p = @camera.translate(point)
 			#print "translated point\n"
 			Gosu::draw_rect(p.x,p.y,width,height,color)
+		end
+		def pixel(point,color=DEFAULT_COLOR)
+			p = @camera.translate(point)
+			#Gosu::draw_line(p.x,p.y,color,p.x,p.y,color)
+			Gosu::draw_rect(p.x,p.y,1,1,color)
+		end
+		# Draws points from a path object.
+		def path(path, color=DEFAULT_COLOR)
+			points = path.points
+			i = 1
+			while i < points.size
+				#pixel(points[i-1])
+				line(points[i-1], points[i], color)
+				#pixel(points[i])
+				i += 1
+			end
 		end
     end
     # Keeps the graphics object focused on a particular game object,
